fe2f7a824b5da972ddb8b9a48cd3d7a59858551f
Sea-LC <SLCdragon@icloud.com>
2024-11-04 15:10:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 function showthediv(var1) {
 console.log(var1)
 // Hide the one dive that is shown
-document.getElementsByClassName("show")[0].className="hide";
+document.getElementById("div1").style.display="none";
+document.getElementById("div2").style.display="none";
+document.getElementById("div3").style.display="none";
+document.getElementById("div4").style.display="none";
 // Hide the one div you sent to this function
-document.getElementById(var1).className="show";
+document.getElementById(var1).style.display="block";
 }
+
+var list=['-','-','-','-','-','-','-','-','-']
+var list=['1','2','3','4','5','6','7','8','9']
+var count=1
+	
+function select(input) {
+
+    if (count % 2 === 0) {
+    document.getElementById(input.id).className = "used1"
+    }
+    else 
+    {
+    document.getElementById(input.id).className = "used2"
+    }
+    count+=1
+	}

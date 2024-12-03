eb29de6fe151004cf27aa2c5e77a722ae82ea159
Sea-LC <SLCdragon@icloud.com>
2024-12-03 17:22:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function showthediv(var1) {
-console.log(var1)
-// Hide the one dive that is shown
-document.getElementById("instructor").style.display="none";
-document.getElementById("contact").style.display="none";
-document.getElementById("quotes").style.display="none";
-document.getElementById("classes").style.display="none";
-document.getElementById("gallery").style.display="none";
-// Hide the one div you sent to this function
-document.getElementById(var1).style.display="block";
+  console.log(var1)
+  // Hide the one dive that is shown
+  document.getElementById("instructor").style.display = "none"
+  document.getElementById("contact").style.display = "none"
+  document.getElementById("quotes").style.display = "none"
+  document.getElementById("classes").style.display = "none"
+  document.getElementById("gallery").style.display = "none"
+  // Hide the one div you sent to this function
+  document.getElementById(var1).style.display = "block"
 }
+
